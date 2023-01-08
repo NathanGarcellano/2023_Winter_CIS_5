77cdb616556e94f2000db2fe778d5b0ca73134af
NathanGarcellano <122234242+NathanGarcellano@users.noreply.github.com>
2023-01-08 14:34:47 -0800

    Moved files

---
+++ Class/CppTemplate/main.cpp
@@ -0,0 +1,37 @@
+/* 
+ * File:   main.cpp
+ * Author: Nathan Garcellano
+ *
+ * Created on January 8, 2023, 1:43 PM
+ * Purpose:  Template to be used for all future Hmwk, Labs, Exams, Projects
+ */
+
+//System Libraries
+#include <iostream>  //Input Output Library
+
+using namespace std;
+//User Libraries
+
+//Global Constants not Variables
+//Science, Math, Conversions, Dimensions
+
+//Function Prototypes
+
+//execution begins here at main
+int main(int argc, char** argv) {
+    //Set random number seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map/Process the Inputs -> Outputs
+    
+    //Display Inputs/Outputs
+    
+    //Clean up memory and files
+    
+    //Exit the program
+    return 0;
+}
+
---
+++ Class/HelloWorld/main.cpp
@@ -0,0 +1,40 @@
+/* 
+ * File:   main.cpp
+ * Author: Nathan Garcellano
+ * Created on January 8, 2023, 1:43 PM
+ * Purpose:  Hello World
+ *      First Program all progammers write when testing their IDE
+ *      for the first time.
+ *      C:Programming Language by
+ *      Dennis Ritchie and Brian Kerningham
+ */
+
+//System Libraries
+#include <iostream>  //Input Output Library
+
+using namespace std;
+//User Libraries
+
+//Global Constants not Variables
+//Science, Math, Conversions, Dimensions
+
+//Function Prototypes
+
+//execution begins here at main
+int main(int argc, char** argv) {
+    //Set random number seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map/Process the Inputs -> Outputs
+    
+    //Display Inputs/Outputs
+    
+    //Clean up memory and files
+    
+    //Exit the program
+    return 0;
+}
+
